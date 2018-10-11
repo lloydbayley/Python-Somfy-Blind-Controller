16d8a5a0583bc8613f56e8f87c9d5245d95f3acb
lloydbayley <lloyd@berowrapc.com>
2018-10-11 19:44:08 +1100

    Revert "Small update to README as its taking me longer to document than I thought."
    This reverts commit 8ceb50d66784c4249e32b98592fc72be9a5a21df.

--- Blind_Control.py
+++ Blind_Control.py
@@ -4,8 +4,6 @@ import sys
 import os
 
 # You may have to create the channels.txt file and need to change the directory that it is in to suit your needs.
-# There is still a lot of debugging statements that print information in this code so you may want to take them out or
-# create a debug = true/false scenario.
 
 # Channel Information for my house
 
@@ -29,10 +27,10 @@ action = sys.argv[2]
 
 # Somfy Telis 4 has 5 channels
 NR_CHANNELS = 5
-
+    
 # timeout after which remote channel flashing has gone
 CHANNEL_TIMEOUT = 6     # seconds
-
+    
 # time interval for normal button presses
 BUTTON_HOLD    = 0.1    # seconds
 BUTTON_TIMEOUT = 0.2 # seconds
@@ -51,11 +49,11 @@ def set_channel(cur_channel, new_channel):
             channel_diff = (new_channel - cur_channel) % NR_CHANNELS
             # we need 1 press more than the channel difference
             for i in range(channel_diff+1):
-		GPIO.setup(2, GPIO.OUT)
+		GPIO.setup(2, GPIO.OUT) 
 		time.sleep (0.1)
 		GPIO.setup(2, GPIO.IN)
 		time.sleep (0.1)
-
+            
 	    # after channel switching wait until channel flashing is over
             time.sleep(CHANNEL_TIMEOUT)
 
@@ -71,25 +69,25 @@ def set_channel(cur_channel, new_channel):
 # Set what to do with the blind
 def set_blind(action):
 
-	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BCM)             
 
 	if action in ("up", "Up", "UP"):
 
 		print "Sending UP command"
-		GPIO.setup(3, GPIO.OUT)
+		GPIO.setup(3, GPIO.OUT)          
 		time.sleep (1)
 		GPIO.setup(3, GPIO.IN)
 
 	elif action in ("down", "Down", "DOWN"):
 
-		GPIO.setup(4, GPIO.OUT)
+		GPIO.setup(4, GPIO.OUT)          
                 time.sleep (1)
                 GPIO.setup(4, GPIO.IN)
 		print "Sending DOWN command"
 
 	elif action in ("stop", "Stop", "STOP"):
-
-		GPIO.setup(14, GPIO.OUT)
+	
+		GPIO.setup(14, GPIO.OUT)         
                 time.sleep (1)
                 GPIO.setup(14, GPIO.IN)
                 print "Sending STOP command"
